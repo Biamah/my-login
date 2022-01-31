920e38919a62e250865aaa676a1066949c5ce544
Biamah <beatriz.souza.costa@outlook.com>
2022-01-31 19:37:39 -0300

    feat: primeiro componente inputname

---
+++ my-login/src/components/input-name/InputName.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './input-name.css';
+
+const InputName = () => {
+    return ( <input type="text" className="input-name" placeholder="Name" /> );
+}
+ 
+export default InputName;
---
+++ my-login/src/components/input-name/InputName.stories.js
@@ -0,0 +1,14 @@
+import InputName from "./InputName";
+
+export default {
+    title: 'InputName',
+    component: InputName
+}
+
+const Template = args => <InputName {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  
+};
+
